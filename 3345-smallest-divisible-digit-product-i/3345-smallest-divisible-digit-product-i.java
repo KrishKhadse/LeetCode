6c6e0008dae8073e21ff6e3d224341794c22bd25
KrishKhadse <krishkhadse06@gmail.com>
2026-08-06 18:17:49 +0530

    Time: 1 ms (100%), Space: 42.6 MB (50.26%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        
+        for(int i=n; i>=n; i++)
+        {
+            int num = i;
+	        int pro=1;
+
+            while(num!=0)
+            {
+                int rem = num % 10;
+                pro= pro*rem;
+                num = num/10;
+            }
+
+            if( pro % t == 0)
+            {
+                return i;
+            }
+
+        }
+        return 0;
+
+    }
+}
