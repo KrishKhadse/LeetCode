295e9ad24cd74ad3c06126821bc091d2d23c29ed
KrishKhadse <krishkhadse06@gmail.com>
2026-08-19 17:28:44 +0530

    Time: 4 ms (4.17%), Space: 268.7 MB (8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,11 +3,9 @@ class Solution {
     {
         //rotating an array by its length brings it back to the original array.Ins uch case k becomes 0
         k = k % nums.length;
-            
-        int i=0;
-        
-        rev(nums,i,nums.length-1);
-        rev(nums,i,k-1);
+                    
+        rev(nums,0,nums.length-1);
+        rev(nums,0,k-1);
         rev(nums,k,nums.length-1);
     }
 
